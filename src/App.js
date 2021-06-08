593a3be1271a7528af570cbb809094f3e829f84b
dutt23 <shatyaki_dutt@hotmail.com>
2021-06-08 20:37:08 +0530

    add map marker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import {useRef, useEffect, useState} from 'react'
+import { useRef, useEffect, useState } from 'react'
 import * as tt from '@tomtom-international/web-sdk-maps'
+import './App.css';
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
 
-const  App = () => {
+
+const App = () => {
   const mapElement = useRef();
   const [map, setMap] = useState({})
   const [longitude, setLongitude] = useState(-0.1278);
   const [latitude, setLatitude] = useState(51.5074);
 
-  const setLocation = () =>{
-  console.log(navigator.geolocation)
-  navigator.geolocation.getCurrentPosition( position  =>{
+  const setLocation = () => {
+    console.log(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(position => {
       setLatitude(position.coords.latitude)
       setLongitude(position.coords.longitude)
     }, (err) => {
@@ -19,44 +21,65 @@ const  App = () => {
     })
   }
   useEffect(() => {
-    setLocation()
+    if (!longitude || !latitude)
+      setLocation()
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
-      stylesVisibility:{
+      stylesVisibility: {
         trafficFlow: true,
         trafficIncidents: true
       },
       center: [longitude, latitude],
-      zoom:14
-  });
-  setMap(map)
-  return () => map.remove()
+      zoom: 14
+    });
+    setMap(map)
+    const element = document.createElement('div')
+    element.className = 'marker'
+
+    const addMarker = () => {
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      }).setLngLat([longitude, latitude]).addTo(map)
+
+      marker.on('dragend', () => {
+        const lngLat = marker.getLngLat()
+        setLongitude(lngLat.lng)
+        setLatitude(lngLat.lat)
+      })
+    }
+
+    addMarker()
+
+    return () => map.remove()
   }, [longitude, latitude])
   return (
-    <div className="App">
-      <div ref={mapElement} className = "map"></div>
-      <div className="searchBar">
-        <h1>Where to ?</h1>
-        <input
-        id= "longitude"
-        type = "text"
-        className = "longitude"
-        placeHolder = "Put in longitude"
-        onChange={(e) =>{
-          setLongitude(e.target.value)
-        }}
-        />
-        <input
-        id= "latitude"
-        type = "text"
-        className = "latitude"
-        placeHolder = "Put in latitude"
-        onChange={(e) =>{
-          setLatitude(e.target.value)
-        }}
-        />
-      </div>
+    <div>
+      {map && <div className="app">
+        <div ref={mapElement} className="map"></div>
+        <div className="searchBar">
+          <h1>Where to ?</h1>
+          <input
+            id="longitude"
+            type="text"
+            className="longitude"
+            placeholder="Put in longitude"
+            onChange={(e) => {
+              setLongitude(e.target.value)
+            }}
+          />
+          <input
+            id="latitude"
+            type="text"
+            className="latitude"
+            placeHolder="Put in latitude"
+            onChange={(e) => {
+              setLatitude(e.target.value)
+            }}
+          />
+        </div>
+      </div>}
     </div>
   );
 }
